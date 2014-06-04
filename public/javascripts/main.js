cb91073ee1d174d5bb757a18331fce20005d47be
Gulsen Yilmaz <gsenol@gmail.com>
2014-06-04 16:16:36 -0700

    obtained vehicle data for sf-muni, route n

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,28 +1,50 @@
-//add the map to the page
-var width = 1500;
-var height = 1000;
+$(document).ready(function(){
 
-var svg = d3.select('body')
-            .append('svg')
-            .attr('width', width)
-            .attr('height', height)
-            .attr('class', 'map');
+	//add containing svg to the page
+	var width = 1500;
+	var height = 1000;
+
+	var svg = d3.select('body')
+                .append('svg')
+                .attr('width', width)
+                .attr('height', height)
+                .attr('class', 'map');
 
 
-d3.json('/assets/streets.json', function(error, json){
-	if(error) {
-		console.log('error loading map');
-	} else {
-		console.log('success');
-		var projection = d3.geo.mercator().scale(300000).center(d3.geo.centroid(json)).translate([width/4, height/2]);
-		var path = d3.geo.path().projection(projection);
-		svg.selectAll('path')
-           .data(json.features)
-           .enter()
-           .append('path')
-           .attr('d', path)
-           .attr('stroke', 'black')
-           .attr('stroke-width', '0.5px');
-	}
+	//add map to the svg container
+	d3.json('/assets/streets.json', function(error, json){
+		if(error) {
+			console.log('error loading map');
+		} else {
+			console.log('success');
+			var projection = d3.geo.mercator().scale(300000).center(d3.geo.centroid(json)).translate([width/4, height/2]);
+			var path = d3.geo.path().projection(projection);
+			svg.selectAll('path')
+               .data(json.features)
+               .enter()
+               .append('path')
+               .attr('d', path)
+               .attr('stroke', 'black')
+               .attr('stroke-width', '0.5px');
+
+            getLocations();
+        }
+	});
+
+	//get vehicle locations from the server
+	var getLocations = function() {
+		$.ajax({
+			url: "http://webservices.nextbus.com/service/publicXMLFeed?command=vehicleLocations&a=sf-muni&r=N&t=1144953500233",
+			type: "GET",
+			dataType: 'xml',
+			success: function(data){
+				console.log('successfully retrieved data');
+				getCoordinates(data);
+			},
+			error: function(error){
+				console.log('error retrieving data', error);
+			}
+		});
+	};
+
 });
-

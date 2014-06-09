d54005b8f6dcb8a774261382f3d6c1085235b3dc
Gulsen Yilmaz <gsenol@gmail.com>
2014-06-09 11:25:13 -0700

    fixed drop down to filter vehicles on the map

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -43,7 +43,6 @@ $(document).ready(function(){
 						populateRoutes(data);
 						populateDropDown();
 						getLocations();
-						setInterval(function(){ getLocations();}, 15000);
 					},
 					error: function(error){
 						console.log('error retrieving routes data', error);
@@ -52,7 +51,6 @@ $(document).ready(function(){
 			};
 
 			var populateRoutes = function(data){
-				console.log('populatedRoutes data: ', data);
 				$(data).find('route').each(function(){
 					var route = $(this).attr('tag');
 					var title = $(this).attr('title');
@@ -65,22 +63,19 @@ $(document).ready(function(){
 
 				var colors = d3.scale.category20();
 				
+				//assign vehicle type and color to each route
 				for(var route in routes){
-					console.log(routes[route]);
-					//assign vehicle type to each route
-					if(routes[route].tag === '59' || routes[route].tag === '60' || routes[route].tag === '61'){
+					if(route === '59' || route === '60' || route === '61'){
 						routes[route].type = 'cable';
-					} else if(routes[route].tag === 'F' || routes[route].tag === 'J' || routes[route].tag === 'KT' || routes[route].tag === 'L' ||
-						routes[route].tag === 'M' || routes[route].tag === 'N' || routes[route].tag === 'NX') {
+					} else if(route === 'F' || route === 'J' || route === 'KT' || route === 'L' ||
+						route === 'M' || route === 'N' || route === 'NX') {
 						routes[route].type = 'train';
 					} else {
 						routes[route].type = 'bus';
 					}
-					//assign color to each route
 					var random = Math.floor(Math.random() * 20);
 					routes[route].color = colors(random);
 				}
-				console.log('routes populated: ', routes);
 			};
 
 			var populateDropDown = function(){
@@ -98,7 +93,6 @@ $(document).ready(function(){
 
 				var type = 0;
 				for(var i = 0; i < routesList.length; i++){
-					console.log(routes[routesList[i]]);
 					if(routes[routesList[i]].type === 'cable'){
 						type = $cables;
 					} else if(routes[routesList[i]].type === 'train'){
@@ -106,7 +100,7 @@ $(document).ready(function(){
 					} else {
 						type = $busses;
 					}
-					type.append('<option>' + routes[routesList[i]].title + '</option>');					
+					type.append('<option class="' + routes[routesList[i]].tag + '">' + routes[routesList[i]].title + '</option>');
 				}
 
 				//enable bootstrap-select
@@ -114,45 +108,67 @@ $(document).ready(function(){
 
 				//add event listener for filtering
 				var $selection = $('.selectpicker');
-				$selection.on('change', filterVehicles);
-				console.log('drop down populated');
+				$selection.on('change', function(){
+					var selectedOptions = $('.selectpicker option:selected');
+					console.log('selectedOptions: ', selectedOptions);
+					filterVehicles(selectedOptions);
+				});
 			};
 
-			var filterVehicles = function(){
-				// var selectedOptions = $('.selectpicker option:selected');
-				// for(var i = 0; i < selectedOptions.length; i++){
-				// 	selectedRoutes.push(selectedOptions[i].className);
-				// }
+			var filterVehicles = function(selected){
+				
+				//reset storage objects to populate only with vehicles on filtered routes
+				vehicleIds = [];
+				vehicles = [];
+				showVehicles(vehicles);
+
+				//reassign selectedRoutes
+				var selection = [];
+				for(var i = 0; i < selected.length; i++){
+					console.log(selected[i].className);
+					selection.push(selected[i].className);
+				}
+				selectedRoutes = selection;
+				//repopulate vehicles on the map
+				getLocations();
 			};
 
             //get vehicle locations from the server
 			var getLocations = function() {
+				console.log('selected Routes', selectedRoutes);
 				var url = 'http://webservices.nextbus.com/service/publicXMLFeed?command=vehicleLocations&a=sf-muni';
 				var time = Math.round(new Date().getTime()/1000);
-				var route = [1, "N", "J"];
+				var route = '';
 
-				for(var i = 0; i < route.length; i++){
-					$.ajax({
-						url: url + "&t=" + time,
-						type: "GET",
-						dataType: 'xml',
-						success: function(data){
-							console.log('successfully retrieved vehicle locations data: ', data);
-							var vehicleInfo = getVehicleInfo(data);
-							showVehicles(vehicleInfo);
-						},
-						error: function(error){
-							console.log('error retrieving data', error);
-						}
-					});
+				if(selectedRoutes.length){
+					for(var i = 0; i < selectedRoutes.length; i++){
+						route = "&r=" + selectedRoutes[i];
+						locationsCall(url, route, time);
+					}
+				} else {
+					locationsCall(url, route, time);
 				}
+
 			};
 
-			var locationsCall = function(){};
+			var locationsCall = function(url, route, time){
+				$.ajax({
+					url: url + route + "&t=" + time,
+					type: "GET",
+					dataType: 'xml',
+					success: function(data){
+						console.log('successfully retrieved vehicle locations data: ', data);
+						var vehicleInfo = getVehicleInfo(data);
+						showVehicles(vehicleInfo);
+					},
+					error: function(error){
+						console.log('error retrieving data', error);
+					}
+				});
+			};
 
 			//parse xml response to populate vehicle locations data
             var getVehicleInfo = function(data){
-                console.log('get vehicle info returned data: ', data);
                 $(data).find('vehicle').each(function(){
                     var route = $(this).attr('routeTag');
                     var id = $(this).attr('id');
@@ -176,58 +192,55 @@ $(document).ready(function(){
                         vehicles[index].lat = $(this).attr('lat');
                     }
                 });
-                console.log('vehicle data populated: ', vehicles);              
                 return vehicles;
             };
 
             //display vehicles on the map
             var showVehicles = function(data){
                 var vehicles = svg.selectAll('circle')
-                   .data(data);
+					.data(data);
 
                 vehicles.transition().ease('linear').duration(3000)
-                    .attr('cx', function(d){return projection([d.lon, d.lat])[0];})
-                    .attr('cy', function(d){return projection([d.lon, d.lat])[1];});
+					.attr('cx', function(d){return projection([d.lon, d.lat])[0];})
+					.attr('cy', function(d){return projection([d.lon, d.lat])[1];});
 
                 vehicles.enter().append('circle')
-                   .attr('cx', function(d){return projection([d.lon, d.lat])[0];})
-                   .attr('cy', function(d){return projection([d.lon, d.lat])[1];})
-                   .attr('r', 10)
-                   .attr('fill', function(d){ return d.color;})
-                   .attr('class', function(d){ return d.route + ' unfiltered';})
-                   .attr('stroke', 'black');
+					.attr('cx', function(d){return projection([d.lon, d.lat])[0];})
+					.attr('cy', function(d){return projection([d.lon, d.lat])[1];})
+					.attr('r', 10)
+					.attr('fill', function(d){ return d.color;})
+					.attr('fill-opacity', 0.9)
+					.attr('class', function(d){ return d.route;})
+					.attr('stroke', 'black');
 
                 vehicles.exit().remove();
 
                 vehicles = svg.selectAll('text')
-                   .data(data);
+					.data(data);
 
                 vehicles.transition().ease('linear').duration(3000)
-                   .attr('x', function(d){return projection([d.lon, d.lat])[0];})
-                   .attr('y', function(d){return projection([d.lon, d.lat])[1]+3;});
+					.attr('x', function(d){return projection([d.lon, d.lat])[0];})
+					.attr('y', function(d){return projection([d.lon, d.lat])[1]+3;});
 
                 vehicles.enter().append('text')
-                   .text(function(d){return d.route;})
-                   .attr('x', function(d){return projection([d.lon, d.lat])[0];})
-                   .attr('y', function(d){return projection([d.lon, d.lat])[1]+3;})
-                   .attr('font-size', '9px')
-                   .attr('text-anchor', 'middle');
+					.text(function(d){return d.route;})
+					.attr('x', function(d){return projection([d.lon, d.lat])[0];})
+					.attr('y', function(d){return projection([d.lon, d.lat])[1]+3;})
+					.attr('font-size', '9px')
+					.attr('font-weight', 'bold')
+					.attr('text-anchor', 'middle');
 
-                console.log('vehicles displayed on the map');
+                vehicles.exit().remove();
             };
 
             //initial call to get routes populated
             getRoutes();
 
-            //initial call to display vehicles
-            // getLocations();
-
             //update vehicle locations periodically
-            // setInterval(function(){
-            //     getLocations();
-            // }, 11000);
+			setInterval(function(){
+				getLocations();
+			}, 15000);
         }
 	});
-
 
 });
